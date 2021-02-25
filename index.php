308ef1b599463527e7124b233e9523968996ba79
Wesley Costa Viana <wesleyc.viana@gmail.com>
2021-02-25 08:22:51 -0300

    Criamos a classe Usuario

--- index.php
+++ index.php
@@ -2,9 +2,21 @@
 
 require_once("config.php");
 
+echo "<br>======= Select from Usuarios =======================================<br><br>";
+
 $sql = new Sql();
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
+
+echo "<br><br><br>========== Busca Usuario de id = 3  =====================================<br><br>";
+
+$root =  new Usuario();
+
+$root->loadById(3);
+
+echo $root;
+
+echo "<br><br><br>======================================================================<br><br>";
 
 ?>
